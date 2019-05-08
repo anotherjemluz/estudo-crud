4bcd7bc26ab611005259975c60fedc39d690354d
Jemima <contatojemima@gmail.com>
2019-05-08 13:39:18 -0300

    upando a .env

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,12 @@
 //  esse arquivo é responsável pelas configurações de conexão c o banco de dados.
 //  ele permite diferentes conxeções para diferentes ambientes (produção, desenvolvimento, testes...)
 //  nesse caso ficamos apenas com as confg de produção e dispensamos as demais
-// const { db } = require('./.env')
+const { db } = require('./.env')
 
 module.exports = {
   // production: { 
   client: 'mysql',
-  connection: { database: 'loljinha_db', user: 'root', password: 'admin' 
-},
+  connection: db,
   pool: {
     min: 2,
     max: 10
